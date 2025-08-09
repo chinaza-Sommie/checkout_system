cccea664433a5d2e45670f1bff2b6e7ca5ec2888
chinaza-Sommie <chinazachukwunweike35@gmail.com>
2025-08-09 17:23:33 +0100

    UI desgin

---
+++ src/components/checkout.js
@@ -0,0 +1,69 @@
+
+
+function ItemsList() {
+  return (
+    <div className="itemsList-container" >
+
+      <div className="itemsList-content itemsList">
+        <div className="itemsList-title"> Items List </div>
+        <div className="eachitem">
+            <div className="itemName">
+                <div id="itemDisplayImage">A</div>
+                <div className="item-Description">
+                    <div>Item A</div>
+                    <div><b>Price:</b> 50p </div>
+                    <div><b>Special Offer:</b> 3 for 1.30 </div>
+                </div>
+            </div>
+
+            
+
+            <button className="additem">
+                add to cart
+            </button>
+        </div>
+
+        <div className="eachitem">
+            <div className="itemName">
+                <div id="itemDisplayImage">A</div>
+                <div className="item-Description">
+                    <div>Item A</div>
+                    <div><b>Price:</b> 50p </div>
+                    <div><b>Special Offer:</b> 3 for 1.50 </div>
+                </div>
+            </div>
+
+            
+
+            <button className="additem">
+                add to cart
+            </button>
+        </div>
+
+      </div>
+
+      <div className="itemsList-content cartdisplay">
+        <div className="itemsList-title cart-title"> Your Cart </div>
+        <div className="eachitem">
+            <div className="itemName">
+                <div id="itemDisplayImage">A</div>
+                <div className="item-Description">
+                    <div>Item A</div>
+                    <div><b>Price:</b> 50p </div>
+                    <div><b>Special Offer:</b> 3 for 1.30 </div>
+                </div>
+            </div>
+
+            
+
+            <div className="additem">
+                
+                
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ItemsList;
---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <div className="checkoutHeader">
+      <div>
+        Checkout System
+      </div>
+    </div>
+  );
+}
+
+export default Header;
