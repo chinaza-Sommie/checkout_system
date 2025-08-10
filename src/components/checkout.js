5b74ebe5a3e6ee6f5c076c31019f73a840bf26d3
chinaza-Sommie <chinazachukwunweike35@gmail.com>
2025-08-10 10:00:52 +0100

    list population

--- src/components/checkout.js
+++ src/components/checkout.js
@@ -1,45 +1,43 @@
 
 
 function ItemsList() {
-    
+    const itemsData = [
+        { product: "A", name: "item A", price: 50, special:{quantity: 3, price: 1.30}},
+        { product: "B", name: "item B", price: 30, special:{quantity: 2, price: 45}},
+        { product: "C", name: "item C", price: 20},
+        { product: "D", name: "item D", price: 15}
+    ];
+
+
+
+
+
   return (
     <div className="itemsList-container" >
 
         <div className="itemsList-content itemsList">
             <div className="itemsList-title"> Items List </div>
-            <div className="eachitem">
-                <div className="itemName">
-                    <div id="itemDisplayImage">A</div>
-                    <div className="item-Description">
-                        <div>Item A</div>
-                        <div><b>Price:</b> 50p </div>
-                        <div><b>Special Offer:</b> 3 for 1.30 </div>
+            {itemsData.map((item) => (
+                <div className="eachitem">
+                    <div className="itemName">
+                        <div id="itemDisplayImage">{item.product}</div>
+                        <div className="item-Description">
+                            <div>{item.name}</div>
+                            <div><b>Price:</b> {item.price}p </div>
+                            {item.special && (
+                                <div><b>Special Offer:</b> {item.special.quantity} for {item.special.price} </div>
+                            )}
+                            
+                        </div>
                     </div>
+
+                    
+
+                    <button className="additem">
+                        add to cart
+                    </button>
                 </div>
-
-                
-
-                <button className="additem">
-                    add to cart
-                </button>
-            </div>
-
-            <div className="eachitem">
-                <div className="itemName">
-                    <div id="itemDisplayImage">A</div>
-                    <div className="item-Description">
-                        <div>Item A</div>
-                        <div><b>Price:</b> 50p </div>
-                        <div><b>Special Offer:</b> 3 for 1.50 </div>
-                    </div>
-                </div>
-
-                
-
-                <button className="additem">
-                    add to cart
-                </button>
-            </div>
+            ))}
 
         </div>
 
