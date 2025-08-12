3f5b5e25df63bf5611425c9463080418c6f7c6e4
chinaza-Sommie <chinazachukwunweike35@gmail.com>
2025-08-12 22:16:57 +0100

    component breakdown

--- src/components/checkout.js
+++ src/components/checkout.js
@@ -1,72 +1,32 @@
-import React, {useState} from "react";
-import {itemsData, itemsDataSet} from "../data/data.js"
 import ItemList from "./itemList.js";
 import Cart from "./cart";
+import { useCart } from "../functions/functions";
 
-function CheckoutPage({cartVisible}) {
 
-    const [cart, setCart] = useState({});
-    const isMobile = window.innerWidth < 651;
+function CheckoutPage({cartVisible}) {   
+    const { cart, addToCart, removeFromCart, itemsData, itemsDataSet, GetEachItemPriceTotal, calculateTotalPrice, isMobile} = useCart();
 
-    const addToCart = (product) => {
-        setCart((prev) => ({
-            ...prev, [product]: (prev[product] || 0) + 1,
-        }));
-    }
-
-    const removeFromCart = (product) => {
-        setCart((prev) => {
-            if(!prev[product]) return prev;
-            const updatedSet = { ...prev, [product]: prev[product] - 1, }
-            if(updatedSet[product] <= 0) delete updatedSet[product];
-            return updatedSet;
-        })
-    }
-
-    const GetEachItemPriceTotal = (product, quantity) => {
-        const productSet = itemsDataSet[product];
-
-        if(!productSet) return 0;
-        
-        if(productSet.special){
-            const numberOfSpecials = Math.floor( quantity / productSet.special.quantity );
-            const remainingqty = quantity % productSet.special.quantity;
-            const specialTotal = (remainingqty * productSet.price) + (numberOfSpecials * productSet.special.price)
-            
-            // console.log(specialTotal.toFixed(2));
-            return specialTotal;
-        }
-        return quantity * productSet.price;
-    }
-
-    const calculateTotalPrice = () => {
-        return Object.entries(cart).reduce(
-            (sum, [product, quantity]) => sum + GetEachItemPriceTotal(product, quantity), 0
-        )
-    }
-
-    
-  return (
-    <div className="itemsList-container" >
-        {isMobile ? ( 
-            cartVisible ? (
-                <Cart cart={cart} addToCart={addToCart} removeFromCart={removeFromCart}
-                eachItemTotal={GetEachItemPriceTotal} itemsDataSet={itemsDataSet} totalPrice={calculateTotalPrice}/>
-                
+    return (
+        <div className="itemsList-container" >
+            {isMobile ? ( 
+                cartVisible ? (
+                    <Cart cart={cart} addToCart={addToCart} removeFromCart={removeFromCart}
+                    eachItemTotal={GetEachItemPriceTotal} itemsDataSet={itemsDataSet} totalPrice={calculateTotalPrice}/>
+                    
+                ) : (
+                    <ItemList itemsData={itemsData} addToCart={addToCart} />
+                )
             ) : (
-                <ItemList itemsData={itemsData} addToCart={addToCart} />
-            )
-        ) : (
-            <>
-                <ItemList itemsData={itemsData} addToCart={addToCart} />
-                <Cart cart={cart} addToCart={addToCart} removeFromCart={removeFromCart}
-                eachItemTotal={GetEachItemPriceTotal} itemsDataSet={itemsDataSet} totalPrice={calculateTotalPrice}/>
-            </>
-        )}
+                <>
+                    <ItemList itemsData={itemsData} addToCart={addToCart} />
+                    <Cart cart={cart} addToCart={addToCart} removeFromCart={removeFromCart}
+                    eachItemTotal={GetEachItemPriceTotal} itemsDataSet={itemsDataSet} totalPrice={calculateTotalPrice}/>
+                </>
+            )}
 
-        
-    </div>
-  );
+            
+        </div>
+    );
 }
 
 export default CheckoutPage;
---
+++ src/functions/functions.js
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+import {itemsData, itemsDataSet} from "../data/data";
+
+export const useCart = () => {
+    const [cart, setCart] = useState({});
+    const [isMobile, setisMobile] = useState(window.innerWidth < 651);
+
+    useEffect(()=>{
+        const resizeHandler = () =>{
+            setisMobile(window.innerWidth < 651);
+        }
+
+        window.addEventListener("resize", resizeHandler);
+ 
+        return () => {
+            window.removeEventListener("resize", resizeHandler);
+        }
+    }, [])
+   
+    const addToCart = (product) => {
+        setCart((prev) => ({
+            ...prev, [product]: (prev[product] || 0) + 1,
+        }));
+    }
+
+    const removeFromCart = (product) => {
+        setCart((prev) => {
+            if(!prev[product]) return prev;
+            const updatedSet = { ...prev, [product]: prev[product] - 1, }
+            if(updatedSet[product] <= 0) delete updatedSet[product];
+            return updatedSet;
+        })
+    }
+
+    const GetEachItemPriceTotal = (product, quantity) => {
+        const productSet = itemsDataSet[product];
+
+        if(!productSet) return 0;
+        
+        if(productSet.special){
+            const numberOfSpecials = Math.floor( quantity / productSet.special.quantity );
+            const remainingqty = quantity % productSet.special.quantity;
+            const specialTotal = (remainingqty * productSet.price) + (numberOfSpecials * productSet.special.price)
+            
+            // console.log(specialTotal.toFixed(2));
+            return specialTotal;
+        }
+        return quantity * productSet.price;
+    }
+
+    const calculateTotalPrice = (cart) => {
+        return Object.entries(cart).reduce(
+            (sum, [product, quantity]) => sum + GetEachItemPriceTotal(product, quantity), 0
+        )
+    }
+    return { cart, addToCart, removeFromCart,itemsData, itemsDataSet, GetEachItemPriceTotal, calculateTotalPrice, isMobile };
+}
+
